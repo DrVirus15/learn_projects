d68f5e30c09c7aab3630cd2052d7de69c2345753
Anton K <drvirus15@gmail.com>
2025-09-21 19:41:01 +0300

    CalculatorPart2

---
+++ KarelDemo/src/com/shpp/p2p/cs/akoskovtsev/assignment11/operators/CloseBracket.java
@@ -0,0 +1,45 @@
+package com.shpp.p2p.cs.akoskovtsev.assignment11.operators;
+
+import com.shpp.p2p.cs.akoskovtsev.assignment11.Operator;
+
+public class CloseBracket implements Operator {
+    /**
+     * Returns the operator symbol.
+     */
+    @Override
+    public String getOperator() {
+        return ")";
+    }
+
+    /**
+     * Returns the number of operands the operator works with.
+     */
+    @Override
+    public int getOperandCount() {
+        return 0;
+    }
+
+    /**
+     * Returns the precedence level of the operator.
+     */
+    @Override
+    public int getPrecedence() {
+        return 0;
+    }
+
+    /**
+     * Indicates whether the operator is left associative.
+     */
+    @Override
+    public boolean isLeftAssociativity() {
+        return false;
+    }
+
+    /**
+     * This method is not applicable for brackets and returns 0.
+     */
+    @Override
+    public double calculate(double... operands) {
+        return 0;
+    }
+}
---
+++ KarelDemo/src/com/shpp/p2p/cs/akoskovtsev/assignment11/operators/Sqrt.java
@@ -0,0 +1,36 @@
+package com.shpp.p2p.cs.akoskovtsev.assignment11.operators;
+
+import com.shpp.p2p.cs.akoskovtsev.assignment11.Operator;
+
+public class Sqrt implements Operator {
+    /**
+     * Returns the operator symbol.
+     */
+    @Override
+    public String getOperator() {
+        return "sqrt";
+    }
+
+    /**
+     * Returns the number of operands the operator works with.
+     */
+    @Override
+    public int getOperandCount() {
+        return 1;
+    }
+
+    @Override
+    public int getPrecedence() {
+        return 3;
+    }
+
+    @Override
+    public boolean isLeftAssociativity() {
+        return false;
+    }
+
+    @Override
+    public double calculate(double... operands) {
+        return Math.sqrt(operands[0]);
+    }
+}
