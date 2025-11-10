d40f0c8b87e5283c195dbb6585961e6c47d1e240
Lenovo <drvirus15@gmail.com>
2025-11-10 19:31:23 +0200

    improved10.11

--- shppCS-B/src/forExam/improved/src/com/shpp/p2p/cs/akoskovtsev/assignment11/Assignment11Part1.java
+++ shppCS-B/src/forExam/improved/src/com/shpp/p2p/cs/akoskovtsev/assignment11/Assignment11Part1.java
@@ -24,7 +24,6 @@ public class Assignment11Part1 {
         OPERATOR_MAP.put("~", new UnaryMinus());
         OPERATOR_MAP.put("(", new OpenBracket());
         OPERATOR_MAP.put(")", new CloseBracket());
-//        OPERATOR_MAP.put(",", new Coma());
         OPERATOR_MAP.put("cos", new Cos());
         OPERATOR_MAP.put("acos", new ACos());
         OPERATOR_MAP.put("sin", new Sin());
@@ -34,14 +33,9 @@ public class Assignment11Part1 {
         OPERATOR_MAP.put("log", new Log());
         OPERATOR_MAP.put("log2", new Log_2());
         OPERATOR_MAP.put("log10", new Log_10());
-//        OPERATOR_MAP.put("min", new Min());
-//        OPERATOR_MAP.put("Min", new Min());
-//        OPERATOR_MAP.put("Max", new Max());
-//        OPERATOR_MAP.put("max", new Max());
         OPERATOR_MAP.put("sqrt", new Sqrt());
         OPERATOR_MAP.put("exp", new Exp());
     }
-
 
     /**
      * Main method to run the program.
@@ -51,9 +45,9 @@ public class Assignment11Part1 {
     public static void main(String[] args) {
 //        String formula = "15/(7-(1+1))*3-(2+(1+1))*15/(7-(200+1))*3-(2+(1+1))*(15/(7-(1+1))*3-(2+(1+1))+15/(7-(1+1))*3-(2+(1+1)))";
         String formula = args[0].replace(" ", "").toLowerCase(); //TODO handle args... null 0 etc.
-        HashMap<String, Double> variables = parseVariables(args);
+        Map<String, Double> variables = parseVariables(args);
 //        System.out.println(formula + " " + variables);
-        LinkedList<String> rpn = parse(formula); // Convert the formula to Reverse Polish Notation (RPN)
+        Deque<String> rpn = parse(formula); // Convert the formula to Reverse Polish Notation (RPN)
         printResult(rpn, variables);             // Calculate and print the result
     }
 
@@ -63,8 +57,8 @@ public class Assignment11Part1 {
      * @param args - Command-line arguments
      * @return - Map of variable names to their values
      */
-    private static HashMap<String, Double> parseVariables(String[] args) {
-        HashMap<String, Double> variables = new HashMap<>();
+    private static Map<String, Double> parseVariables(String[] args) {
+        Map<String, Double> variables = new HashMap<>();
         for (int i = 1; i < args.length; i++) {
             String parse = args[i];
             int index = parse.indexOf('=');
@@ -83,7 +77,7 @@ public class Assignment11Part1 {
      * @param rpn       - the RPN expression as a stack of tokens
      * @param variables - map of variable names to their values
      */
-    private static void printResult(LinkedList<String> rpn, HashMap<String, Double> variables) {
+    private static void printResult(Deque<String> rpn, Map<String, Double> variables) {
         try {
             double result = calculateRPN(rpn, variables); //TODO перевірка на неправильну кількість дужок
             System.out.println(result);
@@ -105,22 +99,14 @@ public class Assignment11Part1 {
      * @param variables - map of variable names to their values
      * @return - the calculated result
      */
-    private static double calculateRPN(LinkedList<String> rpn, HashMap<String, Double> variables)
+    private static double calculateRPN(Deque<String> rpn, Map<String, Double> variables)
             throws NumberFormatException {
         Stack<Double> tokens = new Stack<>();
-        setVariables(variables, rpn);
+        rpn = checkAndPushVariable(variables, rpn);
         while (!rpn.isEmpty()) {
             String token = rpn.pop();
-            if (isOperator(token)) {        // If the token is an operator, perform the operation
-                double[] operand = new double[OPERATOR_MAP.get(token).getOperandCount()];
-                if (tokens.size() < operand.length) throw new EmptyStackException(); // Not enough operands
-                for (int i = operand.length - 1; i >= 0; i--) {
-                    operand[i] = tokens.pop();
-                }
-                tokens.push(OPERATOR_MAP.get(token).calculate(operand)); // Perform the operation
-                if (tokens.peek().isInfinite() || tokens.peek().isNaN()) {
-                    throw new IllegalArgumentException();
-                }
+            if (isOperator(token)) {
+                performTheOperation(token, tokens);
             } else {
                 tokens.push(Double.parseDouble(token));
             }
@@ -128,18 +114,26 @@ public class Assignment11Part1 {
         return tokens.pop();
     }
 
-    /**
-     * Replaces variables in the stack with their corresponding values from the variables map.
-     *
-     * @param variables - map of variable names to their values
-     * @param stack     - stack of tokens (operands and operators)
-     */
-    private static void setVariables(HashMap<String, Double> variables, LinkedList<String> stack) {
-        for (int i = 0; i < stack.size(); i++) {
-            if (variables.get(stack.get(i)) != null) {
-                stack.set(i, variables.get(stack.get(i)) + "");
-            }
+    private static void performTheOperation(String token, Stack<Double> tokens) {
+        double[] operand = new double[OPERATOR_MAP.get(token).getOperandCount()];
+        if (tokens.size() < operand.length) throw new EmptyStackException(); // Not enough operands
+        for (int i = 0; i < operand.length; i++) {
+            operand[operand.length - 1 - i] = tokens.pop();
         }
+        tokens.push(OPERATOR_MAP.get(token).calculate(operand)); // Perform the operation
+        if (tokens.peek().isInfinite() || tokens.peek().isNaN()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static Deque<String> checkAndPushVariable(Map<String, Double> variables, Deque<String> RPN) {
+        Deque<String> stackWithoutVariables = new ArrayDeque<>();
+        while (!RPN.isEmpty()) {
+            String token = RPN.pop();
+            boolean tokenIsVariable = variables.get(token) != null;
+            stackWithoutVariables.offer(tokenIsVariable ? String.valueOf(variables.get(token)) : token);
+        }
+        return stackWithoutVariables;
     }
 
     /**
@@ -148,9 +142,9 @@ public class Assignment11Part1 {
      * @param formula - the input formula as a string
      * @return - the RPN expression as a linked list of tokens
      */
-    private static LinkedList<String> parse(String formula) {
-        LinkedList<String> rpn = new LinkedList<>();// Output list for RPN
-        Stack<String> opStack = new Stack<>();      // Stack for operators
+    private static Deque<String> parse(String formula) {
+        Deque<String> rpn = new ArrayDeque<>();// Output list for RPN
+        Deque<String> opStack = new ArrayDeque<>();      // Stack for operators
         StringBuilder operand = new StringBuilder();// To build multi-character operands
         for (int i = 0; i < formula.length(); i++) {
             if (operand.isEmpty() && formula.charAt(i) == '-') { // Handle unary minus
@@ -181,25 +175,14 @@ public class Assignment11Part1 {
      * @param opStack - stack of operators
      * @param rpn     - output list for RPN
      */
-    private static void handleOperator(String token, Stack<String> opStack, LinkedList<String> rpn) {
+    private static void handleOperator(String token, Deque<String> opStack, Deque<String> rpn) {
         if (token.equals("(")) {
             opStack.push(token);        // Push open bracket onto the stack
             return;
         }
-        if(opStack.isEmpty()){
-            System.out.println("opStack is empty");
-        }
+
         if (token.equals(")") && !opStack.isEmpty()) {
-            String lastToken = opStack.peek();
-            while (!lastToken.equals("(")) {
-                rpn.add(opStack.pop()); // Pop operators to RPN until an open bracket is found
-                if(!opStack.isEmpty()){
-                    lastToken = opStack.peek();
-                }else {
-                    throw new EmptyStackException();
-                }
-            }
-            opStack.pop();              // Remove the open bracket from the stack
+            handleCloseBracket(opStack, rpn);
             return;
         }
         int tokenPrecedence = OPERATOR_MAP.get(token).getPrecedence();      // Get the precedence of the current operator
@@ -208,13 +191,23 @@ public class Assignment11Part1 {
             String operator = opStack.peek();                               // Peek at the top operator on the stack
             int topPrecedence = OPERATOR_MAP.get(operator).getPrecedence(); // Get the precedence of the operator at the top of the stack
             if (operator.equals("(")) break;                                 // Stop if an open bracket is found
-            if (tokenPrecedence < topPrecedence || (tokenPrecedence == topPrecedence && isLeftAssoc)) {
-                rpn.add(opStack.pop());                                     // Pop all higher or equal precedence operators to RPN
-            } else {
-                break;
-            }
+            if (!(tokenPrecedence < topPrecedence || (tokenPrecedence == topPrecedence && isLeftAssoc))) break;
+            rpn.add(opStack.pop());                                     // Pop all higher or equal precedence operators to RPN
         }
         opStack.push(token);                                                // Push the current operator onto the stack
+    }
+
+    private static void handleCloseBracket(Deque<String> opStack, Deque<String> rpn) {
+        String lastToken = opStack.peek();
+        while (!Objects.equals(lastToken, "(")) {
+            rpn.add(opStack.pop()); // Pop operators to RPN until an open bracket is found
+            if (!opStack.isEmpty()) {
+                lastToken = opStack.peek();
+            } else {
+                throw new EmptyStackException();
+            }
+        }
+        opStack.pop();
     }
 
     /**
@@ -234,7 +227,7 @@ public class Assignment11Part1 {
      * @param operand       - the current operand being built
      * @param operatorStack - the stack of operators
      */
-    private static void addToRPN(LinkedList<String> stack, StringBuilder operand, Stack<String> operatorStack) {
+    private static void addToRPN(Deque<String> stack, StringBuilder operand, Deque<String> operatorStack) {
         if (!operand.isEmpty()) {
             if (OPERATOR_MAP.containsKey(operand.toString())) {
                 operatorStack.push(operand.toString());     // If the operand is actually an operator, push it to the operator stack
