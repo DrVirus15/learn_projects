db27b647a72b2e1a967b47eab5e8fd2a8d2e2beb
Lenovo <drvirus15@gmail.com>
2025-11-11 12:27:08 +0200

    improved10.11

---
+++ shppCS-B/src/forExam/orig/src/com/shpp/p2p/cs/akoskovtsev/assignment11/operators/ACos.java
@@ -0,0 +1,51 @@
+package forExam.orig.src.com.shpp.p2p.cs.akoskovtsev.assignment11.operators;
+
+
+import forExam.orig.src.com.shpp.p2p.cs.akoskovtsev.assignment11.Operator;
+
+/**
+ * The class implements the "acos" operator, which calculates the arc cosine of a given value.
+ */
+public class ACos implements Operator {
+    /**
+     *  Returns the string representation of the operator.
+     */
+    @Override
+    public String getOperator() {
+        return "acos";
+    }
+
+    /**
+     * Returns the number of operands required by the operator.
+     */
+    @Override
+    public int getOperandCount() {
+        return 1;
+    }
+
+    /**
+     * Returns the precedence level of the operator.
+     */
+    @Override
+    public int getPrecedence() {
+        return 4;
+    }
+
+    /**
+     * Indicates whether the operator is left associative.
+     */
+    @Override
+    public boolean isLeftAssociativity() {
+        return false;
+    }
+
+    /**
+     * Calculates the arccosine of the given operand.
+     * @param operands An array containing a single operand.
+     * @return The arccosine of the operand.
+     */
+    @Override
+    public double calculate(double... operands) {
+        return Math.acos(operands[0]);
+    }
+}
---
+++ shppCS-B/src/forExam/orig/src/com/shpp/p2p/cs/akoskovtsev/assignment11/operators/Coma.java
@@ -0,0 +1,45 @@
+package forExam.orig.src.com.shpp.p2p.cs.akoskovtsev.assignment11.operators;
+
+import forExam.orig.src.com.shpp.p2p.cs.akoskovtsev.assignment11.Operator;
+
+public class Coma implements Operator {
+    /**
+     * Returns the operator symbol.
+     */
+    @Override
+    public String getOperator() {
+        return ",";
+    }
+
+    /**
+     * Returns the number of operands the operator works with.
+     */
+    @Override
+    public int getOperandCount() {
+        return 1;
+    }
+
+    /**
+     * Returns the precedence level of the operator.
+     */
+    @Override
+    public int getPrecedence() {
+        return 0;
+    }
+
+    /**
+     * Indicates whether the operator is left associative.
+     */
+    @Override
+    public boolean isLeftAssociativity() {
+        return false;
+    }
+
+    /**
+     * This method is not applicable for coma and returns operand[0].
+     */
+    @Override
+    public double calculate(double... operands) {
+        return operands[0];
+    }
+}
